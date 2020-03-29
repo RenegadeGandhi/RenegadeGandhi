2aa94c7c487506899a2e18616227bb477bc25894
Osinaya Oludare <osinayaoludare@gmail.com>
2020-03-30 10:11:30 +1200

    A simple form with NodeJS

---
+++ form.js
@@ -0,0 +1,34 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            console.log(body);
+            res.end('ok');
+        });
+    } 
+    else {
+        res.end(`
+            <!doctype html>
+            <html>
+            <body>
+                <form method="POST" action="/message">
+                <h5>PLEASE ENTER A MESSAGE BELOW</h5>
+                <label class="msg" for="message">Message: </label><br>
+                <input name="message" type="text"><br>
+                <button type="submit" name="submit-button">Submit</button>
+                </form>
+            </body>
+            </html>
+        `);
+    }
+});
+server.listen(8080);
+
+
+fs.writeFileSync('message.txt', server);
